649696be18bc2e14f32afbbd607af983c91fc813
tiisetso000 <154628973+tiisetso000@users.noreply.github.com>
2025-02-19 01:10:37 +0000

    Update mockEvents.ts
    Updates

--- src/data/mockEvents.ts
+++ src/data/mockEvents.ts
@@ -32,41 +32,26 @@ export interface Event {
 
 export const mockEvents: Event[] = [
   {
-    id: "sgidi-fridays-nov-2024",
-    title: "Pretty Girls Love Sgidi Fridays",
-    description: "We bring Sgidi Friday. Join us for an unforgettable experience of live performances, great food, and amazing atmosphere.",
-    date: "Nov 29, 2024",
-    location: "Impala Cafe",
-    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1732926050/sgidi_love_zjvtfa.jpg",
+    id: "soweto-shutdown-nov-2024",
+    title: "SOWETO SHUTDOWN",
+    description: "Sxova present to you SOWETO SHUTDOWN once more, bigger & better this time🥹 this is a musical celebration, celebrating my journey and love we have received throughout. Efforts & lessons. Family & friends we have made outside our homes.",
+    date: "May 03, 2025",
+    location: "Soweto",
+    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1739923058/WhatsApp_Image_2025-02-18_at_23.53.35_a41febf9_gkirkv.jpg",
     price: 99.99,
     startTime: "16:00",
-    endTime: "23:00",
+    endTime: "02:00",
     lineup: [
       {
-        name: "DJ Maphorisa",
+        name: "Sxova",
         facebook: "https://facebook.com/realblackcoffee",
         instagram: "https://instagram.com/realblackcoffee"
       },
       {
-        name: "Kabza De Small",
+        name: "Shaun101",
         facebook: "https://facebook.com/nasty_c",
         instagram: "https://instagram.com/nasty_c"
       },
-      {
-        name: "Uncle Waffles",
-        facebook: "https://facebook.com/realblackcoffee",
-        instagram: "https://instagram.com/realblackcoffee"
-      },
-      {
-        name: "DBN Gogo",
-        facebook: "https://facebook.com/nasty_c",
-        instagram: "https://instagram.com/nasty_c"
-      },
-      {
-        name: "Major League DJz",
-        facebook: "https://facebook.com/realblackcoffee",
-        instagram: "https://instagram.com/realblackcoffee"
-      }
     ],
     socialMedia: {
       instagram: "https://instagram.com/summerfest",
@@ -76,57 +61,47 @@ export const mockEvents: Event[] = [
     ticketTypes: [
       {
         name: "Early Bird",
-        price: 79.99,
+        price: 99.99,
         description: "Limited early access tickets at a special price",
         available: 100
       },
       {
         name: "General Admission",
-        price: 99.99,
-        description: "Standard festival access",
-        available: 0
+        price: 149.99,
+        description: "At the Gate!!",
+        available: 30
       },
       {
         name: "VIP",
-        price: 199.99,
-        description: "VIP area access, complimentary drinks, and exclusive viewing areas",
-        available: 20
+        price: 249.99,
+        description: "VIP area access",
+        available: 70
       }
     ]
   },
   {
     id: "pens-down-nov-2024",
-    title: "Pens Down",
-    description: "Join the biggest pens down chilliars, Everyone is welcome, and networking opportunities.",
-    date: "Nov 30, 2024",
-    location: "Naledi",
-    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1732926130/pensdown_bttpcl.jpg",
-    price: 299.99,
+    title: "Black Tee & Friends",
+    description: "Yeye🤘🏿 hope y'all good,@sportman striker x stiflerman smiso x team phoko phoko  Present's to you The unlock of shakaman Yktv  Mr sgidongooo🔥🥳❤Sharists Please Do The Most 🙏🏾❤😭 & Spread The Word Your support Will be highly appreciated 🫶.",
+    date: "April 19, 2025",
+    location: "Tshepisong",
+    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1739923058/WhatsApp_Image_2025-02-18_at_23.53.36_cfe5827a_wwyhrj.jpg",
+    price: 0.00,
     startTime: "09:00",
     endTime: "18:00",
     lineup: [
       {
-        name: "Black Coffee",
+        name: "Sportsman Striker",
         facebook: "https://facebook.com/realblackcoffee",
         instagram: "https://instagram.com/realblackcoffee"
       },
       {
-        name: "Shimza",
+        name: "Stilfer",
         facebook: "https://facebook.com/nasty_c",
         instagram: "https://instagram.com/nasty_c"
       },
       {
-        name: "DJ Fresh",
-        facebook: "https://facebook.com/realblackcoffee",
-        instagram: "https://instagram.com/realblackcoffee"
-      },
-      {
-        name: "Zakes Bantwini",
-        facebook: "https://facebook.com/nasty_c",
-        instagram: "https://instagram.com/nasty_c"
-      },
-      {
-        name: "Sun-El Musician",
+        name: "Phokophoko",
         facebook: "https://facebook.com/realblackcoffee",
         instagram: "https://instagram.com/realblackcoffee"
       }
@@ -138,58 +113,38 @@ export const mockEvents: Event[] = [
     },
     ticketTypes: [
       {
-        name: "Early Bird",
-        price: 79.99,
+        name: "Pre-Sold",
+        price: 30.00,
         description: "Limited early access tickets at a special price",
         available: 100
       },
       {
         name: "General Admission",
-        price: 99.99,
+        price: 50.00,
         description: "Standard festival access",
         available: 30
       },
       {
-        name: "VIP",
-        price: 199.99,
-        description: "VIP area access, complimentary drinks, and exclusive viewing areas",
-        available: 0
+        name: "CoolerBox",
+        price: 50.00,
+        description: "Small Cooler Box R50",
+        available: 100
       }
     ]
   },
   {
-    id: "amapiano-world-sept-2024",
-    title: "Amapiano To The World",
-    description: "We bring the best Amapiano Event from around the world, with demonstrations from celebrity chefs and wine experts.",
-    date: "September 30, 2024",
-    location: "Johannesburg South",
-    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1732926170/event1_ywebwu.jpg",
-    price: 149.99,
-    startTime: "12:00",
-    endTime: "20:00",
+    id: "gees-fest-apt-2025",
+    title: "Gees Fest",
+    description: "Wassup Good People The Boygees Clothing Proudly Presents The Official Gees Fest (Mabhanzin's Birthday Celebration) Hosted By Malume Spura & Umfana WeHubbly .SAVE THE DATE (05 APRIL) We Promise Y'all Good Vibes With Perfect Blend Of Iconic Culture,Music and Streetwear.🎂🥳🥳🥳🥳🥳🥳🥳 More Details Coming Soon",
+    date: "April 05, 2025",
+    location: "Soweto (Mapetla)",
+    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1739923059/WhatsApp_Image_2025-02-18_at_23.53.36_b6d14b52_ltrag4.jpg",
+    price: 50.00,
+    startTime: "16:00",
+    endTime: "02:00",
     lineup: [
       {
-        name: "Mellow & Sleazy",
-        facebook: "https://facebook.com/realblackcoffee",
-        instagram: "https://instagram.com/realblackcoffee"
-      },
-      {
-        name: "Tyler ICU",
-        facebook: "https://facebook.com/nasty_c",
-        instagram: "https://instagram.com/nasty_c"
-      },
-      {
-        name: "Ch'cco",
-        facebook: "https://facebook.com/realblackcoffee",
-        instagram: "https://instagram.com/realblackcoffee"
-      },
-      {
-        name: "Focalistic",
-        facebook: "https://facebook.com/nasty_c",
-        instagram: "https://instagram.com/nasty_c"
-      },
-      {
-        name: "Young Stunna",
+        name: "LineUp Coming Soon",
         facebook: "https://facebook.com/realblackcoffee",
         instagram: "https://instagram.com/realblackcoffee"
       }
@@ -202,15 +157,15 @@ export const mockEvents: Event[] = [
     ticketTypes: [
       {
         name: "Early Bird",
-        price: 79.99,
+        price: 49.99,
         description: "Limited early access tickets at a special price",
         available: 100
       },
       {
         name: "General Admission",
         price: 99.99,
-        description: "Standard festival access",
-        available: 30
+        description: "After 9pm Entry Ticket",
+        available: 90
       },
       {
         name: "VIP",
@@ -221,101 +176,38 @@ export const mockEvents: Event[] = [
     ]
   },
   {
-    id: "stance-oyslay-2024",
-    title: "Stance ParkOff",
-    description: "Make sure you bring your friends & tell them to tell their friends because we know they are car lovers",
-    date: "January 25, 2025",
-    location: "Molapo Makapane 139",
-    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1736447349/stance_oyslay.jpg",
-    price: 49.99,
-    startTime: "12:00",
-    endTime: "20:00",
+    id: "brandros-mrh-2025",
+    title: "Brandros",
+    description: "Ladies and gents these are the pilots for the all black everything, the connoisseurs of the days groove! The ones to take centre stage and give us memorable moments to last us a lifetime. Let's rock!!!!🔥",
+    date: "March 29, 2025",
+    location: "Johannesburg",
+    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1739923059/WhatsApp_Image_2025-02-18_at_23.53.36_21e69d31_dci6td.jpg",
+    price: 120.00,
+    startTime: "17:00",
+    endTime: "02:00",
     lineup: [
       {
-        name: "DJY_K1_SA",
+        name: "Kabza De Small",
         facebook: "https://facebook.com/realblackcoffee",
         instagram: "https://instagram.com/realblackcoffee"
       },
-      {
-        name: "ADK",
-        facebook: "https://facebook.com/nasty_c",
-        instagram: "https://instagram.com/nasty_c"
-      },
-      {
-        name: "Marikana Fest",
-        facebook: "https://facebook.com/realblackcoffee",
-        instagram: "https://instagram.com/realblackcoffee"
-      },
-      {
-        name: "Sir Trill",
-        facebook: "https://facebook.com/nasty_c",
-        instagram: "https://instagram.com/nasty_c"
-      },
-      {
-        name: "Daliwonga",
-        facebook: "https://facebook.com/realblackcoffee",
-        instagram: "https://instagram.com/realblackcoffee"
-      }
-    ],
-    socialMedia: {
-      instagram: "https://instagram.com/foodexpo",
-      facebook: "https://facebook.com/foodexpo",
-      tiktok: "https://tiktok.com/@foodexpo"
-    },
-    ticketTypes: [
-      {
-        name: "Early Bird",
-        price: 79.99,
-        description: "Limited early access tickets at a special price",
-        available: 0
-      },
-      {
-        name: "General",
-        price: 49.00,
-        description: "Standard festival access",
-        available: 30
-      },
-      {
-        name: "VIP",
-        price: 199.99,
-        description: "VIP area access, complimentary drinks, and exclusive viewing areas",
-        available: 0
-      }
-    ]
-  },
-  {
-    id: "creative-link-sept-2024",
-    title: "Creative Link",
-    description: "We bring the best Creative Link Event from around the world, with demonstrations from celebrity chefs and wine experts.",
-    date: "September 30, 2024",
-    location: "JebbeTown",
-    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1732927962/creative_ioypbg.jpg",
-    price: 149.99,
-    startTime: "12:00",
-    endTime: "20:00",
-    lineup: [
       {
         name: "Kelvin Momo",
-        facebook: "https://facebook.com/realblackcoffee",
-        instagram: "https://instagram.com/realblackcoffee"
-      },
-      {
-        name: "De Mthuda",
         facebook: "https://facebook.com/nasty_c",
         instagram: "https://instagram.com/nasty_c"
       },
       {
-        name: "Lady Du",
+        name: "Oscar Mbo",
         facebook: "https://facebook.com/realblackcoffee",
         instagram: "https://instagram.com/realblackcoffee"
       },
       {
-        name: "Sir Trill",
+        name: "Shakes X Les",
         facebook: "https://facebook.com/nasty_c",
         instagram: "https://instagram.com/nasty_c"
       },
       {
-        name: "Daliwonga",
+        name: "Skroef",
         facebook: "https://facebook.com/realblackcoffee",
         instagram: "https://instagram.com/realblackcoffee"
       }
@@ -328,57 +220,57 @@ export const mockEvents: Event[] = [
     ticketTypes: [
       {
         name: "Early Bird",
-        price: 79.99,
+        price: 0.00,
         description: "Limited early access tickets at a special price",
-        available: 100
+        available: 0
       },
       {
         name: "General Admission",
-        price: 99.99,
+        price: 120.00,
         description: "Standard festival access",
-        available: 30
+        available: 90
       },
       {
         name: "VIP",
-        price: 199.99,
+        price: 250.00,
         description: "VIP area access, complimentary drinks, and exclusive viewing areas",
-        available: 0
+        available: 70
       }
     ]
   },
   {
-    id: "strickly-amapiano-sept-2024",
-    title: "Strickly Amapiano",
-    description: "Free entrance event celebrating Amapiano music",
-    date: "September 30, 2024",
-    location: "Johannesburg South",
-    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1710715583/lalzybbptf8xxraapifg.jpg",
-    price: 0,
+    id: "two-man-may-2025",
+    title: "Two Man",
+    description: "🚨🚨🚨 ON 24 MAY FIASCO PRESENTS TWO MAN SHOW 🚨🚨🚨",
+    date: "May 24, 2025",
+    location: "Location well be shared",
+    image: "https://res.cloudinary.com/ddhdpvywz/image/upload/v1739923058/WhatsApp_Image_2025-02-18_at_23.53.36_39259c04_ffczd6.jpg",
+    price: 40.00,
     startTime: "12:00",
     endTime: "20:00",
     lineup: [
       {
-        name: "Mellow & Sleazy",
+        name: "Nkuleee 501",
         facebook: "https://facebook.com/realblackcoffee",
         instagram: "https://instagram.com/realblackcoffee"
       },
       {
-        name: "Tyler ICU",
+        name: "Rico",
         facebook: "https://facebook.com/nasty_c",
         instagram: "https://instagram.com/nasty_c"
       },
       {
-        name: "Ch'cco",
+        name: "King P",
         facebook: "https://facebook.com/realblackcoffee",
         instagram: "https://instagram.com/realblackcoffee"
       },
       {
-        name: "Focalistic",
+        name: "Sxova",
         facebook: "https://facebook.com/nasty_c",
         instagram: "https://instagram.com/nasty_c"
       },
       {
-        name: "Young Stunna",
+        name: "Cakes",
         facebook: "https://facebook.com/realblackcoffee",
         instagram: "https://instagram.com/realblackcoffee"
       }
@@ -388,6 +280,15 @@ export const mockEvents: Event[] = [
       facebook: "https://facebook.com/foodexpo",
       tiktok: "https://tiktok.com/@foodexpo"
     },
-    ticketTypes: []
-  }
+    ticketTypes: [
+      {
+        name: "Pre Sold",
+        price: 40.00,
+        description: "Limited early access tickets at a special price",
+        available: 100
+      }
+    ]
+  },
+  
+  
 ];
